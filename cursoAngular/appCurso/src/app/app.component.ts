4366e26c3fd2d8159c3ad01d6a3c14f35dd3d7c9
Mario <mario.gallegobecerra.external@atos.net>
2019-03-30 20:49:50 +0100

    visto video 24, ver video 25

--- cursoAngular/appCurso/src/app/app.component.ts
+++ cursoAngular/appCurso/src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Mario';
   destino: string = 'Universo';
+  
 }
--- cursoAngular/appCurso/src/app/app.module.ts
+++ cursoAngular/appCurso/src/app/app.module.ts
@@ -12,7 +12,14 @@ import { ViewmodeloComponent } from './viewmodelo/viewmodelo.component';
 import { EjemetodoComponent } from './ejemetodo/ejemetodo.component';
 import { EjpropertybindingComponent } from './ejpropertybinding/ejpropertybinding.component';
 import { EjeventbindintComponent } from './ejeventbindint/ejeventbindint.component';
-import { EjtwobindingComponent } from './ejtwobinding/ejtwobinding.component'
+import { EjtwobindingComponent } from './ejtwobinding/ejtwobinding.component';
+import { EjdirectivangifComponent } from './ejdirectivangif/ejdirectivangif.component';
+import { EjdirectivangstyleComponent } from './ejdirectivangstyle/ejdirectivangstyle.component';
+import { EjdirectivangclassComponent } from './ejdirectivangclass/ejdirectivangclass.component';
+import { EjdirectivangforComponent } from './ejdirectivangfor/ejdirectivangfor.component';
+import { EjdirectivangswitchComponent } from './ejdirectivangswitch/ejdirectivangswitch.component';
+import { MenuitemDirective } from './menuitem.directive';
+import { EjdirectivaformydirectivaComponent } from './ejdirectivaformydirectiva/ejdirectivaformydirectiva.component'
 
 @NgModule({
   declarations: [
@@ -23,7 +30,14 @@ import { EjtwobindingComponent } from './ejtwobinding/ejtwobinding.component'
     EjemetodoComponent,
     EjpropertybindingComponent,
     EjeventbindintComponent,
-    EjtwobindingComponent
+    EjtwobindingComponent,
+    EjdirectivangifComponent,
+    EjdirectivangstyleComponent,
+    EjdirectivangclassComponent,
+    EjdirectivangforComponent,
+    EjdirectivangswitchComponent,
+    MenuitemDirective,
+    EjdirectivaformydirectivaComponent
   ],
   imports: [
     BrowserModule,
